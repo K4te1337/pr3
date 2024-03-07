a3ab1748d5a6a549e3eac7655f3ba39b4db5c2b5
K4te1337 <katasmirn@rambler.ru>
2024-03-07 18:25:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ThirdFragment.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.myapplication.databinding.FragmentThirdBinding;
+
+public class ThirdFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        FragmentThirdBinding binding = FragmentThirdBinding.inflate(inflater, container,
+                false);
+        binding.button.setOnClickListener(view -> {
+            Bundle result = new Bundle();
+            result.putString("bundleKey", "Данные от другого фрагмента");
+            getParentFragmentManager().setFragmentResult(
+                    "requestKey", result);
+        });
+        return binding.getRoot();
+    }
+}
